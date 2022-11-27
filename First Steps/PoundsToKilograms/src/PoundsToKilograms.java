bc29214669b01c915d23ff03599ef97eb31d2462
Coding_Jack <36029469+CodingPawn@users.noreply.github.com>
2022-11-27 17:33:52 +0200

    quick update

--- First Steps/PoundsToKilograms/src/PoundsToKilograms.java
+++ First Steps/PoundsToKilograms/src/PoundsToKilograms.java
@@ -1,8 +1,9 @@
 public class PoundsToKilograms {
     public static void main(String[] args) {
         double pounds = 200d;
-        double kilograms = pounds * 0.45359237;
-        // Try adding a const variable for kilogram calculus.
+        final double toKilogram = 0.45359237d;
+        double kilograms = pounds * toKilogram;
+
         System.out.printf("%.2f pounds => %.4f kilograms.", pounds, kilograms);
     }
 }
