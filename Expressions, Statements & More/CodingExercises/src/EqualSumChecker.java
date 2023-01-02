64871d20327b2780ee1fc4ce3f8d899d6a78845b
Coding_Jack <36029469+CodingPawn@users.noreply.github.com>
2023-01-02 14:59:58 +0200

    solution added

---
+++ Expressions, Statements & More/CodingExercises/src/EqualSumChecker.java
@@ -0,0 +1,14 @@
+public class EqualSumChecker {
+    public static void main(String[] args) {
+        System.out.println(hasEqualSum(1, 1, 1));  // should return false since 1 + 1 is not equal to 1
+
+        System.out.println(hasEqualSum(1, 1, 2));  // should return true since 1 + 1 is equal to 2
+
+        System.out.println(hasEqualSum(1, -1, 0));  // should return true since 1 + (-1) is 1 - 1 and is equal to 0
+
+
+    }
+    public static boolean hasEqualSum(int x, int y, int z) {
+        return x + y == z;
+    }
+}
