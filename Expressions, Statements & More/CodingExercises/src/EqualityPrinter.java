23a41a24b50d01a856d0dac32ec633ac5cc46854
Coding_Jack <36029469+CodingPawn@users.noreply.github.com>
2023-01-03 21:54:36 +0200

    solution added

---
+++ Expressions, Statements & More/CodingExercises/src/EqualityPrinter.java
@@ -0,0 +1,24 @@
+import com.sun.jdi.Value;
+
+public class EqualityPrinter {
+    public static void main(String[] args) {
+        printEqual(1, 1, 1); // should print text All numbers are equal
+
+        printEqual(1, 1, 2); // should print text Neither all are equal or different
+
+        printEqual(-1, -1, -1); // should print text Invalid Value
+
+        printEqual(1, 2, 3); // should print text All numbers are different
+    }
+    public static void printEqual(int x, int y, int z) {
+        if (x < 0 || y < 0 || z < 0) {
+            System.out.println("Invalid Value");
+        } else if (x == y && x == z) {
+            System.out.println("All numbers are equal");
+        } else if (x != y && x != z && y != z) {
+            System.out.println("All numbers are different");
+        } else {
+            System.out.println("Neither all are equal or different");
+        }
+    }
+}
